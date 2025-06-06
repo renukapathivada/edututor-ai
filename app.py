2bd25273869439274ca9ca3e44204ef434ede92f
Renuka <pathivadarenuka8@gmail.com>
2025-06-06 16:20:54 +0000

    Fix: guard firebase init and ensure database_url

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ from sentence_transformers import SentenceTransformer, util
 # Firebase setup
 import json
 cred = credentials.Certificate(dict(st.secrets["firebase"]))
-firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://edututor-ai-370b5-default-rtdb.asia-southeast1.firebasedatabase.app/"
-})
+# firebase_admin.initialize_app(cred, {
+#     "databaseURL": "https://edututor-ai-370b5-default-rtdb.asia-southeast1.firebasedatabase.app/"
+# })
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": st.secrets["firebase"]["https://edututor-ai-370b5-default-rtdb.asia-southeast1.firebasedatabase.app/"]
+    })
+
 
 st.set_page_config(page_title="EduTutor AI", layout="wide")
 
